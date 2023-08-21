0bc9c3be915ba37fd26626ee84932283e45e115b
jinnianwushuang <35559766+jinnianwushuang@users.noreply.github.com>
2023-08-22 04:00:05 +0800

    时钟 2 完成

--- 时钟1/clock.js
+++ 时钟1/clock.js
@@ -1,9 +1,26 @@
 function myTime(){
   const time = new Date();
-  const hh = time.getHours();
-  const mm = time.getMinutes();
-  const ss = time.getSeconds();
-  document.getElementById('clock').innerText = Math.floor(hh / 10) + (hh % 10 + ':') + Math.floor(mm / 10) + mm % 10 + ':' + Math.floor(ss / 10) + ss % 10;
+  let hh = time.getHours();
+  let mm = time.getMinutes();
+  let ss = time.getSeconds();
+
+  hh = ''+ Math.floor(hh / 10) + (hh % 10 )
+  mm =  (''+mm).padStart(2,'0')
+  ss =   ss<10?'0'+ss :ss
+
+
+
+  let final_str = `${hh}:${mm}:${ss}`
+
+  document.getElementById('clock').innerText = final_str
+ 
 }
-myTime();
-setInterval(myTime,1000);
+// myTime();
+
+
+console.log('页面加载完成测试------------1');
+window.onload=function(){
+  console.log('页面加载完成测试------------2');
+  setInterval(myTime,1000);
+}
+console.log('页面加载完成测试------------3');
